3fca6a092f34f0e0ef53346a0962360efd2bbfde
audiph <lagarde.marcjoseph@gmail.com>
2025-08-06 23:31:41 +0800

    cleanup and add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import { AppSidebar } from '@/common/components/app-sidebar';
-import { ChartAreaInteractive } from '@/common/components/chart-area-interactive';
 import { DataTable } from '@/common/components/data-table';
-import { SectionCards } from '@/common/components/section-cards';
-import { SiteHeader } from '@/common/components/site-header';
-import { SidebarInset, SidebarProvider } from '@/common/components/ui/sidebar';
-import Image from 'next/image';
 import data from '@/data.json';
 
 export default function Home() {
@@ -12,10 +6,6 @@ export default function Home() {
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <SectionCards />
-          <div className="px-4 lg:px-6">
-            <ChartAreaInteractive />
-          </div>
           <DataTable data={data} />
         </div>
       </div>
